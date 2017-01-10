5c5b39753171a102b0e6656027e9404b2f059982
arthur-alvesaf <arthur.alvesaf@gmail.com>
2017-01-10 09:35:24 -0600

    pruebas unitarias funcionales en /parcial1/unitarias/prueba

---
+++ parcial1/unitarias/first_unit.cc
@@ -0,0 +1,13 @@
+#include "first.h"
+#include "gtest/gtest.h"
+
+// To compile g++ -isystem ../../googletest/include/ -pthread first.cc first_unit.cc gtest_main.a -o prueba
+
+TEST(Factorial, positivos) {
+	ASSERT_EQ(1, factorial(1));
+	ASSERT_EQ(120, factorial(5));
+	ASSERT_EQ(1, factorial(0));
+}
+
+TEST(Factorial, negativos) {
+}
